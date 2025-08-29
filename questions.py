5630ca1abb267fdd9b26fb0dca4dc8756d534a13
Kalin <82368495+kotseleeeeen@users.noreply.github.com>
2025-08-29 14:54:45 +0800

    Update questions.py
    Fixed docstrings to make them more descriptive and consistent

--- questions.py
+++ questions.py
@@ -1,22 +1,42 @@
+'''
+FINAL PROJECT: Text-Based Quiz/Trivia Game
+
+Submitted by: PROJECT GROUP H
+Mary Christen Gemel Tolentino
+Dylan Isaac Vela
+Ann Carleen Gaile Cuevas
+Aria Carmela De Vera
+Ryan Jay Buenaventura
+Megan Isabel Sharpe
+
+Submitted to:
+Asst. Prof. Katrina Joy M. Abriol-Santos
+
+Text-Based Trivia Game: Questions Module
+This class represents the questions module of the Trivia Game
+'''
+
 class Questions:
      '''
-     The questions for the quiz
-     hindi na gagawa ng list for the question, because we'll be creating a new instance of the Question class per question na sasagutin. tama ba?
+     Represents questions in the Text-Based Trivia Game
      '''
 
      def __init__(self, question_text, answer, correct_answers, category):
-          self.question_text = question_text                     # string
-          self.__answer = answer                                 # string
-          self.__correct_answers = correct_answers or []         # string <list> or depende sa sagot
-          self.__category = category                             # string, this part allows the users to pick a category to play on
+          '''
+          Initializes a new instance of the Questions class.
+        
+          Parameters:
+          question_text: The question text of the Trivia Game             
+          answer: The answer to the question in the Trivia Game
+          correct_answers: The correct answers to the question in the Trivia Game
+          category: The category of the questions in the Trivia Game
+          '''
           
-          """
-          # this line of code could be reserved for main.py
-
-          if correct_answers not in self.__answer:
-               raise ValueError (f"Wrong answer! The answer was: ", self.__correct_answers  )
-          """
-
+          self.question_text = question_text                     # string attribute
+          self.__answer = answer                                 # string attribute
+          self.__correct_answers = correct_answers or []         # string attribute or list (depending on the answers)
+          self.__category = category                             # string attribute
+          
      def get_question_text(self):
           ''' Returns the question text of the game. '''
           return self.question_text
@@ -39,6 +59,7 @@ class PopCultureQuestions (Questions):
     Inherits from the Question class and returns category as "Pop Culture"
     '''
     def __init__(self, question_text, answers, correct_answer):
+         # the super function allows the PopCultureQuestions to inherit all the methods and attributes of the Questions class
         super().__init__(question_text, answers, correct_answer, category="Pop Culture")
                          
 class AnimeQuestions (Questions):
@@ -47,6 +68,7 @@ class AnimeQuestions (Questions):
     Inherits from the Question class and returns category as "Anime"
     '''
     def __init__(self, question_text, answers, correct_answer):
+         # the super function allows the AnimeQuestions to inherit all the methods and attributes of the Questions class
         super().__init__(question_text, answers, correct_answer, category="Anime")
 
 class GameQuestions (Questions):
@@ -55,14 +77,5 @@ class GameQuestions (Questions):
     Inherits from the Question class and returns category as "Game"
     '''
     def __init__(self, question_text, answers, correct_answer):
+         # the super function allows the GameQuestions to inherit all the methods and attributes of the Questions class
         super().__init__(question_text, answers, correct_answer, category="Game")
-
-
-''' This segment just tests one of the subclasses! It works naman on my end so if ever move na toh kay main.py she works >:3 '''
-
-test = PopCultureQuestions(question_text="Ayie", answers="A", correct_answer="B")
-
-print(test.get_category())                   # returns as Pop Culture
-print(test.get_question_text())              # returns as Ayie
-print(test.get_answer())                     # returns as A
-print(test.get_correct_answers())            # returns as B
